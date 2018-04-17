4574bd1eb39febdd750ffd3b00e4db98d1208eb3
lovZgump <1029533012@qq.com>
2018-04-17 20:50:57 +0800

    add bottom_dialog in addCommActivity

--- app/src/main/java/com/example/clayou/sechandtransdemo/AddCommActivity.java
+++ app/src/main/java/com/example/clayou/sechandtransdemo/AddCommActivity.java
@@ -1,10 +1,17 @@
 package com.example.clayou.sechandtransdemo;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -13,6 +20,7 @@ import java.util.List;
 public class AddCommActivity extends AppCompatActivity {
 
     private Spinner selCategory;
+    private ImageView img;
 
     private List<String> categoryList;
 
@@ -50,6 +58,60 @@ public class AddCommActivity extends AppCompatActivity {
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
 
+            }
+        });
+
+
+        img = findViewById(R.id.img);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopDialog();
+            }
+        });
+    }
+
+    private void showPopDialog() {
+        final Dialog bottomDialog = new Dialog(this, R.style.BottomDialog);
+        View contentView = LayoutInflater.from(this).inflate(R.layout.dialog_content_normal, null);
+        bottomDialog.setContentView(contentView);
+        ViewGroup.LayoutParams layoutParams = contentView.getLayoutParams();
+        layoutParams.width = getResources().getDisplayMetrics().widthPixels;
+        contentView.setLayoutParams(layoutParams);
+        bottomDialog.getWindow().setGravity(Gravity.BOTTOM);
+
+        // 设置显示动画
+        bottomDialog.getWindow().setWindowAnimations(R.style.BottomDialog_Animation);
+
+        // 设置点击外围取消
+        //bottomDialog.setCanceledOnTouchOutside(true);
+        bottomDialog.show();
+
+        Button btn_gallery = bottomDialog.getWindow().findViewById(R.id.btn_gallery);
+        Button btn_photo = bottomDialog.getWindow().findViewById(R.id.btn_photo);
+        Button btn_cancel = bottomDialog.getWindow().findViewById(R.id.btn_cancel);
+
+
+        btn_gallery.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("InlinedApi")
+            @Override
+            public void onClick(View view) {
+                bottomDialog.dismiss();
+            }
+        });
+
+        btn_photo.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("InlinedApi")
+            @Override
+            public void onClick(View view) {
+                bottomDialog.dismiss();
+            }
+        });
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomDialog.dismiss();
             }
         });
     }
